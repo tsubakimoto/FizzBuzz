cc481ce22f21f36131e21a48d7dfca3c3f4308e2
tsubakimoto <tkmbmbr@gmail.com>
2015-02-20 06:37:16 +0900

    add solution

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string Say(int n)
+        {
+            if (n < 1)
+                throw new System.ArgumentOutOfRangeException();
+
+            if (n % 3 == 0 && n % 5 == 0)
+                return "Fizz Buzz";
+
+            if (n % 3 == 0)
+                return "Fizz";
+
+            if (n % 5 == 0)
+                return "Buzz";
+
+            return n.ToString();
+        }
+    }
+}
